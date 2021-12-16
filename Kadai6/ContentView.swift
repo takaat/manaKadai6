d47a21ddf132815d7715effc4ab5432d1c0dec5d
mana.u <great8life@gmail.com>
2021-12-16 23:41:41 +0900

    SecondCommit

--- Kadai6/ContentView.swift
+++ Kadai6/ContentView.swift
@@ -8,29 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject private var judgement = Judgement()
+    @State private var targetValue = Int.random(in: 1...100)
     @State private var inputValue: Float = 50
+    @State private var isCorrectAnswer = false
     @State private var isShowAlert = false
 
     var body: some View {
         VStack(spacing: 50.0) {
-            Text("\(judgement.targetValue)")
+            Text("\(targetValue)")
                 .font(.largeTitle)
-            Text("入力値\(Int(inputValue))")
+
             Slider(value: $inputValue, in: 1...100,
                    label: {}, minimumValueLabel: {Text("1")}, maximumValueLabel: {Text("100")})
                 .padding(/*@START_MENU_TOKEN@*/.horizontal/*@END_MENU_TOKEN@*/)
 
-            Button(action: {
-                judgement.result(inputValue: inputValue)
-                isShowAlert = true
-            }, label: {Text("判定！")})
+            Button(action: {judgment()}, label: {Text("判定！")})
         }
         .alert("結果", isPresented: $isShowAlert,
-               actions: {Button(action: {judgement.retry();inputValue = 50},
-                                label: {Text("再挑戦")})},
-               message: {judgement.isCorrectAnswer ?
+               actions: {Button(action: {retry()}, label: {Text("再挑戦")})},
+               message: {isCorrectAnswer ?
             Text("あたり！\nあなたの値：\(Int(inputValue))") : Text("はずれ！\nあなたの値：\(Int(inputValue))")})
+    }
+
+    private func judgment() {
+        if Int(inputValue) == targetValue {
+            isCorrectAnswer = true
+        }
+        isShowAlert = true
+    }
+
+    private func retry() {
+        targetValue = Int.random(in: 1...100)
+        inputValue = 50
+        isCorrectAnswer = false
     }
 }
 
